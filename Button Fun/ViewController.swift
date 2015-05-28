d9adcea4fa154799f8556ae358e7bb26c2b3694e
Andrew Watts <wattsap@appstate.edu>
2015-05-28 16:43:45 -0400

    First commit

--- Button Fun/ViewController.swift
+++ Button Fun/ViewController.swift
@@ -10,16 +10,20 @@ import UIKit
 
 class ViewController: UIViewController {
 
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+    @IBOutlet weak var statusLabel: UILabel!
+    @IBAction func buttonPressed(sender: UIButton) {
+        let title = sender.titleForState(.Normal)!
+        let plainText="\(title) button pressed"
+        let styledText = NSMutableAttributedString(string: plainText)
+        let attributes = [
+            NSFontAttributeName:
+                UIFont.boldSystemFontOfSize(statusLabel.font.pointSize)
+        ]
+        let nameRange = (plainText as NSString).rangeOfString(title)
+        styledText.setAttributes(attributes, range: nameRange)
+        statusLabel.attributedText = styledText
     }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
-    }
-
+    
 
 }
 
